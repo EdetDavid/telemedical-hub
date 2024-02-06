8a705526541cce5b265487e57b44f1f319885a75
Dvooskid <dvooskid1234@gmail.com>
2024-02-06 07:52:52 +0100

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -144,7 +144,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 # STATIC_ROOT =
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
